460ae3d35118bbbf14b96aaaaf1d3cb3438cb2db
Terppa <terosoi@yahoo.co.uk>
2015-09-11 12:57:46 +0300

    dokumentaatio korjaus

--- config/environment.sh
+++ config/environment.sh
@@ -3,5 +3,5 @@
 # korvaa tämä omalla käyttäjätunnuksellasi
 USERNAME="terosoik"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER="e-keittokirja"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
